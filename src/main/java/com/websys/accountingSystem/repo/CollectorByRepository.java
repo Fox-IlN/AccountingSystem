f840196aaa6b6ff4de0ddcd6fc98afd04a456282
Олег Кораблёв <o.korablev@bk.ru>
2023-11-03 12:46:06 +0300

    release

--- src/main/java/com/websys/accountingSystem/repo/CollectorByRepository.java
+++
@@ -1,9 +0,0 @@
-/*
-package com.websys.accountingSystem.repo;
-
-import com.websys.accountingSystem.models.CollectorBy;
-import org.springframework.data.repository.CrudRepository;
-
-public interface CollectorByRepository extends CrudRepository<CollectorBy, Long> {
-}
-*/
